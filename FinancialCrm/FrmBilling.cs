e1eef0188d85db4439bb78122834f009a066a05b
Kadir-Hali <kadirhali07@gmail.com>
2025-02-24 17:39:51 +0300

    24.02.25-2

---
+++ FinancialCrm/FrmBilling.cs
@@ -0,0 +1,84 @@
+﻿using FinancialCrm.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinancialCrm
+{
+    public partial class FrmBilling : Form
+    {
+        public FrmBilling()
+        {
+            InitializeComponent();
+        }
+        FinancialCrmDbEntities db = new FinancialCrmDbEntities();
+
+        private void FrmBilling_Load(object sender, EventArgs e)
+        {
+            var values = db.Bills.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnBillList_Click(object sender, EventArgs e)
+        {
+            var values = db.Bills.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnCreateBill_Click(object sender, EventArgs e)
+        {
+            Bills bills = new Bills();
+            bills.BillTitle = txtBillTitle.Text;
+            bills.BillAmount = decimal.Parse(txtBillAmount.Text);
+            bills.BillPeriod = txtBillPeriod.Text;
+            db.Bills.Add(bills);
+            db.SaveChanges();
+            MessageBox.Show("Ödeme Başarılı Bir Şekilde Sisteme Eklendi", "Ödeme & Faturalar", MessageBoxButtons.OK,MessageBoxIcon.Information);
+            var values = db.Bills.ToList();
+            dataGridView1.DataSource = values;
+
+        }
+
+        private void btnRemoveBill_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtBillId.Text);
+            var removeValue = db.Bills.Find(id);
+            db.Bills.Remove(removeValue);
+            db.SaveChanges();
+            MessageBox.Show("Ödeme Başarılı Bir Şekilde Sistemden Silindi", "Ödeme & Faturalar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            var values = db.Bills.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnUpdateBill_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtBillId.Text);
+            var updatedValues = db.Bills.Find(id);
+            updatedValues.BillTitle = txtBillTitle.Text;
+            updatedValues.BillAmount = decimal.Parse(txtBillAmount.Text);
+            updatedValues.BillPeriod = txtBillPeriod.Text;
+            db.SaveChanges();
+            MessageBox.Show("Ödeme Başarılı Bir Şekilde Sistemde Güncellendi", "Ödeme & Faturalar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            var values2 = db.Bills.ToList();
+            dataGridView1.DataSource = values2;
+        }
+
+        private void btnBanksForm_Click(object sender, EventArgs e)
+        {
+            FrmBanks frm = new FrmBanks();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
